8aea67f863146661d29d7f4f5c0337b2851f4ad4
heyhoje <heyoje325@gmail.com>
2023-05-31 15:13:06 +0900

    day2 숙제

---
+++ Java/src/day2_04_Homework/Ex1.java
@@ -0,0 +1,10 @@
+package day2_04_Homework;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java/src/day2_04_Homework/Ex2.java
@@ -0,0 +1,8 @@
+package day2_04_Homework;
+
+public class Ex2 {
+	public static void main (String[] args ) {
+		
+	}
+
+}
