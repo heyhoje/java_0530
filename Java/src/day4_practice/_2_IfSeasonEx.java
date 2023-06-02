decdc0b798aaef305ebdfb515bc1ac1843970667
heyhoje <heyoje325@gmail.com>
2023-06-02 14:38:32 +0900

    java 4일차 조건문 예제

---
+++ Java/src/day4_practice/_2_IfSeasonEx.java
@@ -0,0 +1,44 @@
+package day4_practice;
+
+import java.util.Scanner;
+
+public class _2_IfSeasonEx {
+
+	public static void main(String[] args) {
+		// 월을 입력받아 입력받은 월의 계절을 출력하세요.
+		// 3, 4, 5 : 봄 / 6, 7, 8 : 여름 / 9, 10, 11 : 가을 / 12, 1, 2 : 겨울 / 그 외 : 잘못된 월 
+		
+		int month; // 변수선언
+		//String Season;
+		Scanner scan = new Scanner(System.in); // 입력 = 스캐너
+		System.out.println("input month : "); // 안내문구 : 월을 입력하세요.
+		month = scan.nextInt(); // 여기까지는 많이했죠? 이정도는 할 수 있죠? ㅇㅇ넵
+		
+		// if문은 참/거짓 으로 판별되야함. <비교> 또는 <논리연산자>가 들어가야한다 // 같다(==) 연산자도 써야함
+		//0 ~ 13 까지 테스트해보기 
+		if(month == 3 || month == 4 || month == 5) { // 크다작다로 가야할까???   //Type mismatch: cannot convert from int to boolean
+													// The operator == is undefined for the argument type(s) boolean, int
+			System.out.println(month + "월은 봄입니다.");
+		}else if (month == 6 || month == 7 || month == 8) {
+			System.out.println(month + "월은 여름입니다.");
+		}else if (month == 9 || month == 10 || month == 11) {
+			System.out.println(month + "월은 가을입니다.");
+		}else if (month == 12 || month == 1 || month == 2) {
+			System.out.println(month + "월은 겨울입니다.");
+		}else {
+			System.out.println("잘못된 월입니다.");
+		}
+		
+		System.out.println("--------------------");
+		if (month >= 4 && month <= 5) { // 봄 : 3이거나(==같거나) 4이거나 5이거나 (3이상이고 5이하이면)
+			System.out.println(month + "월은 봄입니다.");
+		}else if (month == 12 || (month >= 1 && month <= 2)) {// 겨울 : (월이 12이거나, 월이 1이상이고 2이하이면)
+			System.out.println(month + "월은 겨울입니다."); // 논리연산자는 우선순위가 없어서 (동작순서에따라) 앞에서부터 묶을경우 문제가 될 수 있다.
+														// 먼저 실행해야할 연산자에 괄호를 쳐주는게 좋다. 
+			// 논리연산자에서 서로 다른 연산자가 섞여있는 경우, 괄호를 쳐서 작업해주는게 좋다!
+		}
+		
+		
+	}
+
+}
---
+++ Java/src/day4_practice/_6_IfScoreEx.java
@@ -0,0 +1,40 @@
+package day4_practice;
+
+import java.util.Scanner;
+
+public class _6_IfScoreEx {
+
+	public static void main(String[] args) {
+		//성적을 입력받아 성적에 맞는 학점을 출력하는 코드를 작성하세요
+//		90이상 ~100이하 : A
+//		80이상 ~ 90미만 : B
+//		70이상 ~ 80미만 : C
+//		60이상 ~ 70미만 : D
+//		60미만 : F
+//		0보다 작거나 100보다 큰 경우 : wrong score
+		
+		int score;
+		Scanner sc = new Scanner(System.in); // 스캐너는 기본. 복붙. 많이써보면 노력안해도 익숙해짐
+		System.out.println("성적을 입력하세요.");
+		score = sc.nextInt();
+		
+		if(score < 0 || score > 100) {
+			System.out.println("wrong score!");
+		} else if (score >= 90) { // && score <= 100) <- 100이하도 안써도 됐음(위에서 100이상을 쳐냈기 때문에)
+			System.out.println(score + "점은 A학점 입니다.");
+		} else if (score >= 80) {
+			System.out.println(score + "점은 B학점 입니다.");
+		} else if (score >= 70) {
+			System.out.println(score + "점은 C학점 입니다.");
+		} else if (score >= 60) {
+			System.out.println(score + "점은 D학점 입니다.");
+		} else if (score < 60) {
+			System.out.println(score + "점은 F학점 입니다.");
+		}
+			
+		
+		
+		
+	}
+
+}
