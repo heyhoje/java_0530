76a26392f8d041990790936efd5151d89ac8f9df
heyhoje <heyoje325@gmail.com>
2023-06-05 15:01:41 +0900

    5일차 과제 문제

---
+++ Java/src/day5/homework/_2_UpDownEx.java
@@ -0,0 +1,12 @@
+package day5.homework;
+
+public class _2_UpDownEx {
+
+	public static void main(String[] args) {
+//		2. UpDown게임
+//		1~100사이의 랜덤한 정수를 생성하고, 이 정수를 맞추는 게임을 구현하세요. *RandomEx 활용
+//		랜덤한 수 : 45
+//		input : 50 > Down! >> input : 40 > UP! >> input : 45 > Good! //[종료] 맞출때까지
+	}
+
+}
