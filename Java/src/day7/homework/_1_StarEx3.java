b0c9e02b0ecf83ef1c837f3e3e502d6feefc947b
heyhoje <heyoje325@gmail.com>
2023-06-09 12:46:29 +0900

    7일차 과제 완성!

--- Java/src/day7/homework/_1_StarEx3.java
+++ Java/src/day7/homework/_1_StarEx3.java
@@ -7,13 +7,14 @@ public class _1_StarEx3 {
 		System.out.println("----다이아몬드----");
 		int i = 0;
 		int j = 0;
-		/*  sss*  	 	i = 1 공=3 *=1 
-		 *  ss** *		i = 2 공=2 *=3 
-		 *  s*** **		i = 3 공=1 *=5 
-		 *  **** ***	i = 4 공=0 *=7 
-		 *  s*** ** 	i = 5 공=1 *=5 
-		 * 	ss** *		i = 6 공=2 *=3 
-		 *  sss*		i = 7 공=3 *=1
+		int k = 0;
+		/*  sss*  	 	  i = 1 공=3 *=1 
+		 *  ss** *		  i = 2 공=2 *=3 
+		 *  s*** **		  i = 3 공=1 *=5 
+		 *  **** ***	//i = 4 공=0 *=7 
+		 *  s*** ** 	  i = 5 공=1 *=5 
+		 * 	ss** *		  i = 6 공=2 *=3 
+		 *  sss*		  i = 7 공=3 *=1
 		 * 
 		 */
 		/* 외부반복문
@@ -21,16 +22,34 @@ public class _1_StarEx3 {
 		 * 규칙성(실행문) : * i개를 출력 후 엔터
 		 * */
 		
-		for (i = 1; i <= 7; i++){
-			for (j = 1; j <= 4 - i; j++) {
+		for (i = 0; i < 4; i++) {
+			/* 내부반복문
+			 * 반복횟수 : j는 1부터 5 - i / i까지, 1씩 증가
+			 * 규칙성(실행문) : ' ' / * 를 출력
+			 * */
+			for (j = 1; j <= 3 - i; j++) {
 				System.out.print(" ");
 			}
-			for (j = 0; j <= 8; j += 2) {
+			for (j = 1; j <= i + 1; j++) {
+				System.out.print("*");
+			}// * i개 출력
+			for (j = 1; j <= i; j++) { // ** 추가된 for문!
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-				
+		for (i = 0; i < 3; i++) {
+			for (k = 1; k <= i + 1; k++) {
+				System.out.print(" ");
+			}
+			for (k = 1; k <= 3 - i; k++) {
+				System.out.print("*");
+			}
+			for (k = 1; k <= 2 - i ; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 
 }
