ccb301344eb9eeb51d3d8141666414b268ea55f7
heyhoje <heyoje325@gmail.com>
2023-06-10 15:05:01 +0900

    (3일차 연습문제 수정)

--- Java/src/day3_practice/Ex2.java
+++ Java/src/day3_practice/Ex2.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 public class Ex2 {
 
 	public static void main(String[] args) {
-		/*1.성적을 입력받아 입력받은 성적이 60점 이상인지 확인하는 코드를 작성하세요.
-		​예 :  
-		input score : 
-		65
-		[결과출력] Do you pass 65 points? pass
-				​
-		input score : 
-		45
-		[결과출력] Do you pass 45 points? fail
-		*/
+		/* 2. 성적을 입력받아 입력받은 성적이 60점이상인지 확인하는 코드를 작성하세요.
+		 * 예:
+		 * input score :
+		 * 65
+		 * Do you pass 65 points? pass
+		 * input score :
+		 * 45
+		 * Do you pass 45 points? fail
+		 * */
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Input score : ");
 		int score = sc.nextInt(); 
 		
-		String result = score >= 60 ? "pass" : "fail";// in result 가 아닌 String 변수로 받아야함(pass/fail을 값으로 받아야해서)
+		String result = score >= 60 ? "pass" : "fail";// in result 媛� ���� String 蹂���濡� 諛����쇳��(pass/fail�� 媛��쇰� 諛����쇳�댁��)
 		System.out.println("Do you pass " + score + " points? : " + result);
 		
 		sc.close();
