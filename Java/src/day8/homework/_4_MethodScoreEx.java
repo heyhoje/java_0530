3d5e4f26a87f8eb0b8ed328e9be3d3f40fa542f5
heyhoje <heyoje325@gmail.com>
2023-06-12 15:20:29 +0900

    8일차 과제 성적산출
    이거도 안됨

--- Java/src/day8/homework/_4_MethodScoreEx.java
+++ Java/src/day8/homework/_4_MethodScoreEx.java
@@ -12,28 +12,30 @@ public class _4_MethodScoreEx {
 		System.out.println("성적을 입력하세요 : ");
 		score = sc.nextInt();
 		
+		
 		grade(score);
 		
 		sc.close();
 	}
 	
 	/** 기능 : 성적을 입력하면 학점을 알려준다. 
-	 * 매개변수 : 성적 => int score 
+	 * 매개변수 : 성적 => double score 
 	 *	리턴타입 : 학점 문자열
 	 *	매소드명 : grade 
 	 */
-	public static void grade(int num) {
-		int score = num; // 0도 1도 아닌, 매개변수 num이었다네 어쩐지 안썼더라...는 몰랐다
+	public static void grade(int score) {
+		
+		score; // 0도 1도 아닌, 매개변수 num이었다네 어쩐지 안썼더라...는 몰랐다
 		// String grade;
 		if (score < 0 || score > 100) {
 			System.out.println("Wrong Score");
-		}else if (score >= 90 && score <= 100) {
+		}else if (score >= 90) {
 			System.out.println("A 입니다");
-		}else if (score >= 80 && score < 90) {
+		}else if (score >= 80) {
 			System.out.println("B 입니다");
-		}else if (score >= 70 && score < 80) {
+		}else if (score >= 70) {
 			System.out.println("C 입니다");
-		}else if (score >= 60 && score < 70) {
+		}else if (score >= 60) {
 			System.out.println("D 입니다");
 		}else if (score < 60) {
 			System.out.println("F 입니다");
