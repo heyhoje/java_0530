5cbebf7f1d143cf7a02523da50ab8c97a1e7e07d
heyhoje <heyoje325@gmail.com>
2023-06-14 14:56:15 +0900

    java 배열 예제

---
+++ Java/src/day11_array/_1_ArrayEx1.java
@@ -0,0 +1,39 @@
+package day11_array;
+
+import java.util.Scanner;
+
+public class _1_ArrayEx1 {
+
+	public static void main(String[] args) {
+		/* 학생들의 국어 성적을 저장하기 위한 배열을 생성하세요
+		 * 학생들은 총 10명
+		 */
+		
+		double [] studentKoreaScore = new double[3];
+		
+		//*double std1, std2, std3;
+		  Scanner sc = new Scanner(System.in);
+		
+		for (int i = 0; i < 3; i++) {
+			 System.out.println("input : ");
+			 studentKoreaScore[i] = sc.nextDouble();
+			 //ㄴ하나의 변수처럼 사용됨. [번지]
+		}
+		for (int i = 0; i < 3; i++) {
+			System.out.println(studentKoreaScore[i]); //자동으로 기본값으로 초기화됨
+		}
+		
+		// 학생 3명의 국어 점수 평균을 구하고 출력하는 코드를 작성하세요.
+		 
+		double sum = 0.0;
+		double average;
+		for (int i = 0; i < 3; i++) {
+			sum += studentKoreaScore[i];
+		}
+		average = sum / 3;
+		System.out.println("average : " + average);
+		
+		sc.close();
+	}
+
+}
