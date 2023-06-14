289e54931c36b58107966f100449e172e172d40a
heyhoje <heyoje325@gmail.com>
2023-06-14 14:59:48 +0900

    11일차 과제 문제

---
+++ Java/src/day11/homework/_1_BassballGame.java
@@ -0,0 +1,15 @@
+package day11.homework;
+
+public class _1_BassballGame {
+
+	public static void main(String[] args) {
+		// 1. 숫자 야구 게임을 구현하세요
+				// 1 ~ 9 사이의 중복되지 않은 세 수를 맞추는 게임
+				// 규칙 : 
+				/* B : 같은 숫자가 있지만 위치가 다름
+				 * S : 같은 숫자가 있고 위치가 같음
+				 * 3O : 일치하는 숫자가 하나도 없음
+				 * */
+	}
+
+}
