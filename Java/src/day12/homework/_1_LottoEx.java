d90eb28df36676f208e0455268ef4e64b9f99bb1
heyhoje <heyoje325@gmail.com>
2023-06-16 00:57:46 +0900

    12일차 과제 풀이 고민(메인영역)
    단어장 복습 남음

--- Java/src/day12/homework/_1_LottoEx.java
+++ Java/src/day12/homework/_1_LottoEx.java
@@ -1,9 +1,16 @@
 package day12.homework;
 
+import java.util.Scanner;
+
+import array.Array;
+
 public class _1_LottoEx {
 
 	public static void main(String[] args) {
-		// 1. 로또 번호를 생성하고, 사용자가 번호를 입력하면 몇등인지 출력하는 코드를 작성하세요
+		// 1. 로또 번호(45개)를 생성하고, 
+		// 2. 사용자가 번호를 입력하면 
+		// 3. 몇등인지 출력하는 코드를 작성하세요
+		
 		/* 1등 : 번호 6개
 		 * 2등 : 번호 5개 + 보너스
 		 * 3등 : 번호 5개
@@ -14,5 +21,52 @@ public class _1_LottoEx {
 		// computer : 보너스 포함 7개
 		// user : 6개ㅑ
 
+		// 1. 뽑아야할 숫자 7개, 6개를 랜덤으로 생성(중복x) 
+		int lotto[] = new int[6]; // 보너스도 숫자니까 7개 뽑는거지?ㅇㅇ
+		//int bonus[] = new int[1];
+		int user[] = new int[6]; // lotto가 뽑은 숫자랑 몇개 같은지, 순서 상관없이
+		
+		int min = 1, max = 45; // 번호는 1부터 45까지 
+		int normal = 0; // 기본 6개
+		int bonus = 0; // 보;너스 1개
+		
+		// 콘솔창 입력
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("이번주 로또 번호 6개!");
+		Array.createRandomArray(min, max, lotto); 
+		Array.printArray(lotto);
+		System.out.println("보너스 번호");
+		// 마지막 나온 숫자를 보너스라고해?? 보너스만 따로 뽑아야할거같은데
+		//Array.createRandomArray(min, max, bonus); 
+		//Array.printArray(bonus);
+		
+		// ㄴ 이렇게 하면 로또번호랑 보너스번호랑 중복발생.. 
+		// 어떻게 보너스를 보너스라고 알려줘서 2등을 골라내지?
+		// ㄴ 로또가 뽑은 번호 제외하고, 하나만 더 뽑아달라고 하면 안됨???
+		
+		// 2+3. 반복문(여러번 입력하고 판별해야함)
+//		do {
+//			// 2. 사용자가 숫자 3개를 입력
+//			System.out.println("user : ");
+//			for(int i = 0; i < user.length; i++) {
+//				user[i] = sc.nextInt();
+//			}
+//			// 사용자 입력 체크/중복되면 판별할 필요 없음
+//			if(Array.arrayCheck(user)) {
+//				System.out.println("중복된 수를 입력하지 마세요!");
+//				continue;
+//			}
+//		
+//			// 3. 판별
+//			normal = 0;
+//			bonus = 0;
+//			
+//		} while();
+			
+		System.out.println("축하합니다. 등 입니다");
+		sc.close();
 	}
+	
+	
 }
--- Java/src/day12/practice/_1_VocabularyNoteEx1.java
+++ Java/src/day12/practice/_1_VocabularyNoteEx1.java
@@ -8,6 +8,7 @@ public class _1_VocabularyNoteEx1 {
 		// 1. 영어 단어장을 만들기 위해 필요한 객체를 만들고 테스트 해보세요. 
 		// 아직 배열 안쓰고/ 최종적으로는 단어장을 만든다구요?
 		
+		//클래스명 객체명 = new 클래스명(매개변수?멤버변수?)
 		Word word1 = new Word("vocabulary", "the words that make up a language");
 		
 		word1.print();
