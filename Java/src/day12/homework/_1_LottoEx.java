4622b9d3ff2cccf1a40690cf033045c64b01553b
heyhoje <heyoje325@gmail.com>
2023-06-15 14:50:53 +0900

    12일차 과제 문제

---
+++ Java/src/day12/homework/_1_LottoEx.java
@@ -0,0 +1,18 @@
+package day12.homework;
+
+public class _1_LottoEx {
+
+	public static void main(String[] args) {
+		// 1. 로또 번호를 생성하고, 사용자가 번호를 입력하면 몇등인지 출력하는 코드를 작성하세요
+		/* 1등 : 번호 6개
+		 * 2등 : 번호 5개 + 보너스
+		 * 3등 : 번호 5개
+		 * 4등 : 번호 4개
+		 * 5등 : 번호 3개
+		*/
+		
+		// computer : 보너스 포함 7개
+		// user : 6개ㅑ
+
+	}
+}
