b715b6a3e6ec8ba674e4cd2b055bf85671212d75
heyhoje <heyoje325@gmail.com>
2023-06-16 13:29:15 +0900

    13일차 과제 문제(영어 단어장 프로그램)

---
+++ Java/src/day13/homework/_1_VocabularyNoteEx.java
@@ -0,0 +1,32 @@
+package day13.homework;
+
+public class _1_VocabularyNoteEx {
+
+	public static void main(String[] args) {
+		// day12.practice 활용. 더 실제로 사용 가능하도록....
+		
+		// 1. 영어 단어장 프로그램을 구현하세요
+		// Word, VocabularyNote 클래스 이용
+		
+		/*메뉴
+		 * 1. 단어 추가
+		 * 2. 단어 삭제
+		 * 3. 단어 수정
+		 * 	 a. 단어 수정
+		 *   b. 뜻 수정
+		 *   c. 뜻 삭제
+		 * 4. 단어 출력
+		 *   a. 전체 출력
+		 *   b. 검색
+		 * 5. 프로그램 종료
+		 */
+		
+//		메뉴문 출력
+//		메뉴 선택
+//		서브메뉴 나옴
+//		서브메뉴선택
+//		수정할 단어 / 수정될 단어 
+//		수정에 성공했습니다
+	}
+
+}
