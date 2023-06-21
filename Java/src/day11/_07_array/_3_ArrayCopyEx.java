ecae80cfa2e0fe664b537d0653a72e64dfc958f4
heyhoje <heyoje325@gmail.com>
2023-06-22 01:09:38 +0900

    day11 배열복사(얕은복사/깊은복사) 복습 및 질문

--- Java/src/day11/_07_array/_3_ArrayCopyEx.java
+++ Java/src/day11/_07_array/_3_ArrayCopyEx.java
@@ -1,6 +1,6 @@
 package day11._07_array;
 
-// system.arraycopy
+// system.arraycopy(arr1, 복사할 첫번째 위치, arr2, 붙여넣기할 첫번째 위치, 복사할 개수)
 
 public class _3_ArrayCopyEx {
 
@@ -21,11 +21,12 @@ public class _3_ArrayCopyEx {
 		System.out.println("[arr2 초기값으로]");
 		arr2[0] = 1; //초기값으로 되돌림
 		
-		arr2 = new int[arr1.length]; // 배열을 새로 하나 만들고????
-									 // 복사하기 위한 배열을 생성. arr1의 크키만큼
+		arr2 = new int[arr1.length]; // arr1의 크키만큼 배열을 새로 하나 만들기
+									 // 복사하기 위한 배열을 생성. 
 		
 		
-		//반복문으로 복사 => 어떤 그림이 진행되는거죠????????
+		// 반복문으로 복사 => 어떤 그림이 진행되는거죠????????
+		// maybe.. 새로운 배열공간을 만들어놓고, 다시 같은 인스턴스 주소값을 넣는것 같죠?? 맞을까?
 		for(int i = 0; i < arr1.length; i++) {
 			arr2[i] = arr1[i];
 		}
@@ -39,15 +40,18 @@ public class _3_ArrayCopyEx {
 			System.out.println(tmp);
 		} 
 		
-		System.out.println("[arr3 생성]");
-		int arr3[] = new int[arr1.length];
-		System.arraycopy(arr1, 2, arr3, 2, 3);
+		// 이게 깊은 복사가 아니고 얕은 복사라는거죠? 
 		
-		arr3[0] = 10;
+		System.out.println("[arr3 생성]");
+		int arr3[] = new int[arr1.length]; // arr1의 크키만큼 배열을 새로 하나 만들고
+		System.arraycopy(arr1, 2, arr3, 2, 3); // 2번지 부터 복사 > 2번지부터 붙여넣기 > 요소 3개만
+		
+		arr3[0] = 10; // 0번지는 10으로 정해줌
 		
 		System.out.println("-----얕은복사------");
 		for(int tmp : arr3) {
-			System.out.println(tmp);
+			System.out.println(tmp); // 이렇게하면 1번지는 비어있는 상태인 0이고, 
+									 // {10, 0, 3, 4, 5}가 된다는것? 도 얕은복사???
 		} 
 		
 	}
--- Java/src/day11/_07_array/_4_ArrayCopyEx2.java
+++ Java/src/day11/_07_array/_4_ArrayCopyEx2.java
@@ -1,21 +1,22 @@
 package day11._07_array;
 
-//�ȵǴ°� ���ƴµ�(. -> ,) �����Ҹ��� �𸣰���
+//안되는건 고쳤는데(. -> ,) 무슨소린지 모르겠음
 
 public class _4_ArrayCopyEx2 {
 
 	public static void main(String[] args) {
-		// int�迭�� �ƴ� ���� �ڷ�������
+		// int배열이 아닌 '참조 자료형'으로
 //		String names[] = new String[5];
 //		
-//		names[0] = "ȫ�浿";
-//		names[1] = "�Ӳ���";
+//		names[0] = "홍길동";
+//		names[1] = "임꺽정";
 //
-//		//���� for�� (�ڷ��� ������ : �迭�̸�)
+//		//향상된 for문 (자료형 변수명 : 배열이름)
 //		for (String name : names) {
 //			System.out.println(name);
 //		}
-		//Point �迭
+		
+		// Point 배열
 		Point pointArr[] = new Point[5];
 		
 		pointArr[0] = new Point(0,0);
@@ -25,40 +26,42 @@ public class _4_ArrayCopyEx2 {
 		pointArr[4] = new Point(-5,-5);
 	
 	
-		//���� for��(
+		// 향상된 for문(
 		for(Point pt : pointArr) {
 			pt.print();
 		}
 	
-		//�����ϱ� ���� �迭(2�� ����� ����)
+		// 복사하기 위한 배열(2를 만들기 위해)
 		Point pointArr2[] = new Point[5];
 	
-		//pointArr�� �ִ� ������ pointArr2�� ����
-		System.arraycopy(pointArr, 0, pointArr2, 0, pointArr.length);
-		//pointArr2�� �ִ� ù��° ���� 100,0���� �̵�
+		// pointArr에 있는 점들을 pointArr2에 복사
+		System.arraycopy(pointArr, 0, pointArr2, 0, pointArr.length); // 얕은 복사
+		// pointArr2에 있는 첫번째 점을 100,0으로 이동
 		pointArr2[0].move(100,0);
 	
 		System.out.println("----Arr2 move-----");
-		//���� for��
+		//향상된 for문
 		for(Point pt : pointArr) {
 			pt.print();
 		}
 	
+		// >>> 얕은 복사를 했기 떄문에  arr1의 0번지가 (100,0)이 된건가???
+		
 		for(int i = 0; i < pointArr.length; i++) {
-			// pointArr2[i] = pointArr[i]; // ���� ����, �̰� �ƴ϶�
-			pointArr2[i] = new Point(pointArr[i]); // ���� ���� / ���ο� ��ü�� ���� ��¼�� �ؾ���..........
+			// pointArr2[i] = pointArr[i]; // 얕은 복사, 이게 아니라
+			pointArr2[i] = new Point(pointArr[i]); // 깊은 복사 / 새로운 객체를 만들어서??? 어쩌구??? 해야함..........
 	
 		}
 		
-		pointArr2[0].move(-100, -100);
+		pointArr2[0].move(-100, -100); // 새로운 배열공간을 만들어서 arr2의 0번지 값을 옮긴것이기 때문에
 	
-		System.out.println("---------");
-		for(Point pt : pointArr) {
+		System.out.println("---------"); // arr1의 배열 값은 변함이 없다. 라는 것이 맞을까요? 
+		for(Point pt : pointArr) { 
 			pt.print();
 		}
 }
 	
-	
+// class Point 클래스의 정보를 그대로 복사해와서 위의 예제에 활용한것
 }
 class Point{
 	private int x, y;
@@ -70,7 +73,7 @@ class Point{
 	
 	
 
-	//���� ������
+	//복사 생성자
 	public Point(Point pt) {
 		x = pt.x;
 		y = pt.y;
