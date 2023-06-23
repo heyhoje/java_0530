5b21873f31a21a93ad0d626bc1b67aa88f59f699
heyhoje <heyoje325@gmail.com>
2023-06-23 14:58:15 +0900

    18일차 과제(문제)

---
+++ Java/src/day18/homework/ArithmeticEx.java
@@ -0,0 +1,15 @@
+package day18.homework;
+
+public class ArithmeticEx {
+
+	public static void main(String[] args) {
+		// 1. 산술연산을 하는 프로그램을 작성하세요(인터페이스 이용)
+		/* public interface Program{
+		 *   void run()
+		 *   void print Menu();
+		 *   void runMenu(int menu)
+		 * }
+		 */
+	}
+
+}
---
+++ Java/src/day18/homework/ImgEx.java
@@ -0,0 +1,15 @@
+package day18.homework;
+
+public class ImgEx {
+
+	public static void main(String[] args) {
+		// 2. 파일명을 입력받아 파일이 이미지인지 확인하는 코드를 작성하세요.
+		// 이미지 확장자 : jpg, bmp, png(더 있지만 간단하게 3개만 체크)
+		/* input : dog.jpg
+		 * => dog.jpg : [출력] Image
+		 * input : dog.exe
+		 * => dog.exe : [출력] Not image
+		 * */
+	}
+
+}
