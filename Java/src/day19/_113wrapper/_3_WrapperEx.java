a35b2e0efb2a7c9b468f5c90640e8e830c1da717
heyhoje <heyoje325@gmail.com>
2023-06-26 15:09:14 +0900

    java ch11.Wrapper 클래스 예제
    책정리 봐야할듯. 박싱/언박싱 다시보니 초면이네

--- Java/src/day19/_113wrapper/_3_WrapperEx.java
+++ Java/src/day19/_113wrapper/_3_WrapperEx.java
@@ -7,7 +7,7 @@ public class _3_WrapperEx {
 	public static void main(String[] args) {
 		// 1. 기본
 		Integer num1 = 10; // 박싱 : 정수 10을 Integer 클래스 객체 num1에 저장
-		num1 = null;
+		// num1 = null;
 		int num2 = 0;
 		if(num1 != null) {// 예외처리 필요
 			num2 = num1; // 언박싱 : Integer클래스 객체 num1에 있는 값을 num2에 저장. 
