2d00c484ee81f0033bd853cf1df3d16bc7b22b4d
heyhoje <heyoje325@gmail.com>
2023-06-27 15:04:49 +0900

    java ch12. 학생 관리 프로그램 예제(List 이용)
    복습하고 질문할거 정리해야할듯

---
+++ Java/src/day20/practice/_1_StudentEx.java
@@ -0,0 +1,42 @@
+package day20.practice;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import day20._122_collection.Student;
+
+public class _1_StudentEx {
+
+	public static void main(String[] args) {
+		// Student 클래스를 이용하여 학생을 등록/수정/삭제하는 기능의 프로그램을 작성하세요
+		/* 1. 학생 추가
+		 * 2. 학생 삭제
+		 * 3. 학생 수정
+		 * 4. 프로그램 종료
+		 * 메뉴 선택 :  
+		 * */
+//		int menu;
+//		
+//		Scanner sc = new Scanner(System.in);
+//		//System.out.print("메뉴를 입력하세요 : ");
+//		ArrayList<StudentC> list = new ArrayList<StudentC>();
+//		
+//	public void run() {
+//		int menu = -1;
+//		final int EXIT = 4;
+//	
+//	do {
+//		printMenu();
+//	
+//		menu = sc.nextInt();
+//		
+//		runMenu(menu);
+//		
+//	}while(menu != EXIT);
+//		
+//	System.out.println("프로그램 종료!");
+//		
+//	sc.close();
+	}
+
+}
---
+++ Java/src/day20/practice/student/controller/Program.java
@@ -0,0 +1,9 @@
+package day20.practice.student.controller;
+
+public interface Program {
+	
+	void run();
+	void printMenu();
+	void runMenu(int menu);
+
+}
