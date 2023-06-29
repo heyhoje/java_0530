b0625f20121a5aabb3ff73a366c21c42b38b743d
heyhoje <heyoje325@gmail.com>
2023-06-29 14:11:44 +0900

    java ch13.람다식 예제

---
+++ Java/src/day22/lambda/_131_LambdaEx1.java
@@ -0,0 +1,30 @@
+package day22.lambda;
+
+public class _131_LambdaEx1 {
+
+	public static void main(String[] args) {
+		InterfaceA a = new InterfaceA() {
+
+			@Override
+			public int getMax(int num1, int num2) {
+				return num1 >= num2 ? num1 : num2;
+			}
+		};
+		System.out.println(a.getMax(10, 20));
+		
+		InterfaceA b = (num1, num2)->{ // 인터페이스의 값을 쓰기 때문에, 자료형타입이 생략된다
+			return num1 >= num2 ? num1 : num2;
+		};
+		System.out.println(b.getMax(10, 20));
+		
+		// 매개변수가 1개이면 ()를 생략.
+		// 실행문이 1줄이면 {}, return 을 생략 가능!
+		InterfaceA c = (num1, num2)-> num1 >= num2 ? num1 : num2;
+		System.out.println(b.getMax(10, 20));
+	}
+}
+
+interface InterfaceA {
+	int getMax(int num1, int num2);
+
+}
