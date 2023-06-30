4f9f80e666f66e30c21d56709dadd5ee6ae7462f
heyhoje <heyoje325@gmail.com>
2023-06-30 15:19:30 +0900

    (22일차 주말 과제 run)

---
+++ Java/src/day22/homework/manager/student/run/Run.java
@@ -0,0 +1,19 @@
+package day22.homework.manager.student.run;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// 고등학생을 관리하는 프로그램을 작성하세요
+		// - 학생 관리(입학, 전학(오는 학생, 가는 학생), 졸업, 진학
+		// - 성적 관리
+		// - 면담
+		/* 1. 기능을 구체적으로 정리(요구사항 명세서, interface)
+		 * 2. 필요한 클래스를 추가(vo 패키지에 추가)
+		 * 3. 기능을 하나씩 구현
+		 */
+		
+		ManagerStudent ms = new ManagerStudent();
+		ms.run();
+	}
+
+}
