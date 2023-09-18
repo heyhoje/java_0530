8f9731c8d74dba8a539aa74ca5ee643f98946c42
오제 <heyoje325@gmail.com>
2023-09-18 14:00:17 +0900

    SPRING 복습2 : ajax 연습 예제

---
+++ spring projects/edu11/src/main/java/kr/kh/edu/controller/HomeController.java
@@ -0,0 +1,76 @@
+package kr.kh.edu.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.kh.edu.vo.MemberVO;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model, String id) {
+		System.out.println("id : " + id);
+		return "/main/home";
+	}
+	
+	@GetMapping("/{id}")
+	public String id(@PathVariable("id") String id2) {
+		System.out.println("id2 : " + id2);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/ajax/test")
+	public String ajaxTest() {
+		
+		return "main/ajax";
+	}
+	
+	@ResponseBody
+	@PostMapping("/ajax/test1")
+	public Map<String, Object> ajaxTest1(@RequestBody MemberVO member){
+		Map<String, Object> map = new HashMap();
+		System.out.println("test1 : " + member);
+		
+		map.put("member", member);
+		return map;
+	}
+	
+	@ResponseBody
+	@PostMapping("/ajax/test2")
+	public Map<String, Object> ajaxTest2(@RequestParam("me_id") String id, 
+			@RequestParam("me_pw") String pw){
+		Map<String, Object> map = new HashMap();
+		System.out.println("test2 : " + id + ", " + pw);
+		MemberVO member = new MemberVO();
+		
+		member.setMe_id(id);
+		member.setMe_pw(pw);
+		map.put("member", member);
+		return map;
+	}
+	
+	@ResponseBody
+	@PostMapping("/ajax/test3")
+	public MemberVO ajaxTest3(@RequestParam("me_id") String id, 
+			@RequestParam("me_pw") String pw){
+		System.out.println("test3 : " + id + ", " + pw);
+		MemberVO member = new MemberVO();
+		
+		member.setMe_id(id);
+		member.setMe_pw(pw);
+		return member;
+	}
+	
+}
---
+++ spring projects/edu11/src/main/webapp/WEB-INF/views/main/home.jsp
@@ -0,0 +1,17 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!doctyoe html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<a href="<c:url value='?id=abc'/>">?를 이용하여 전송</a> <br>
+	<a href="<c:url value='/abc'/>">경로에 데이터를 전송</a>
+	
+<script>
+	
+</script>
+</body>
+</html>
