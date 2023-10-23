9718611ae618ae4bc82191193b767f2497e97f40
오제 <heyoje325@gmail.com>
2023-10-23 19:14:29 +0900

    (파일옮김)

--- Java/src/review_0723/practice/VocabularyEx1.java
+++
@@ -1,8 +0,0 @@
-package review_0723.practice;
-
-public class VocabularyEx1 {
-	public static void main(String[] args) {
-		Word word = new Word{"vocabulary", "the words that make up a language"};
-	}
-
-}
--- react/board/src/Header.js
+++ react/board/src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+
 function Header(){
 	return(
 		<header className='menu'>
