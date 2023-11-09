bc5cb92c63c429cba841f0aad47fb4addbb0a6b0
오제 <heyoje325@gmail.com>
2023-11-09 16:30:00 +0900

    API 공공데이터 : 에러 확인(서비스키, url 주소 하위항목 선택)
    서비스키 뒤에 \r\n + 빈문자열 삭제
    상세설명 - 상세기능 목록 바꾸면 url 뒤에 경로 바뀜*주의
    "serviceKey" 를 하면 문자열 서비스키를 보낸것.... 변수 serviceKey를 불러올 수 있도록 바꿔준다.

--- spring projects/study/src/main/java/kr/kh/study/controller/APIController2.java
+++ spring projects/study/src/main/java/kr/kh/study/controller/APIController2.java
@@ -14,8 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class APIController2 {
 	
-	private String serviceKey = "N%2BpuuxU3gWDSEZeHQqsedd51t0EYAtuPDLnIYDX8cH1mqrLKtLNU4qwK2n2tmwuWQdCtYrCbgW2Xx68Xz8ie5w%3D%3D\r\n"
-			+ "";
+	private String serviceKey = "N%N%2BpuuxU3gWDSEZeHQqsedd51t0EYAtuPDLnIYDX8cH1mqrLKtLNU4qwK2n2tmwuWQdCtYrCbgW2Xx68Xz8ie5w%3D%3D%3D%3D";
 	
 	@GetMapping("/api/index")
 	public String Test() {
@@ -27,9 +26,9 @@ public class APIController2 {
 	public String airPollution(@RequestParam("location")String location) throws Exception{
 		
 		
-		// url에 parameter 정보들을 추가하시오. 
-		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "serviceKey"); /*Service Key*/
+		// url에 parameter 정보들을 추가하시오. http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty
+		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + serviceKey); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("returnType","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*xml 또는 json*/
         urlBuilder.append("&" + URLEncoder.encode("sidoName","UTF-8") + "=" + URLEncoder.encode(""+location, "UTF-8")); /*한 페이지 결과 수(조회 날짜로 검색 시 사용 안함)*/
         
